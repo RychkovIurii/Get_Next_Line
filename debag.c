3164df4b1d86add25f9d88d537e65447537e6193
Iurii Rychkov <irychkov@student.hive.fi>
2024-05-17 15:54:59 +0300

    Update debug

--- debag.c
+++ debag.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debag.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 15:54:38 by irychkov          #+#    #+#             */
+/*   Updated: 2024/05/17 15:54:42 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <unistd.h>
 #include <fcntl.h>
@@ -21,87 +33,6 @@ char	*free_stuff(char **ptr)
 		*ptr = NULL;
 	}
 	return (NULL);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	size_t	i;
-	char	*dst;
-	size_t	src_len;
-
-	i = 0;
-	if (!s1)
-		return (NULL);
-	src_len = ft_strlen_buf(s1);
-	dst = (char *)malloc(sizeof(char) * (src_len + 1));
-	if (dst == NULL)
-		return (NULL);
-	while (s1[i])
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
-size_t	ft_newline_counter(char *stack)
-{
-	size_t	counter;
-	size_t	i;
-
-	i = 0;
-	counter = 0;
-	while (stack[i])
-	{
-		if (stack[i] == '\n')
-			counter++;
-		i++;
-	}
-	return (counter);
-}
-
-char	*ft_strlcat_buf(char *stack, char *buffer, size_t bytes_read)
-{
-	ssize_t		i;
-	ssize_t		j;
-	char		*new_stack;
-
-	i = 0;
-	j = 0;
-	new_stack = NULL;
-	if (bytes_read <= 0)
-		return (stack);
-	i = ft_strlen_buf(stack);
-	new_stack = (char *)malloc((i + bytes_read + 1) * sizeof(char));
-	if (!new_stack)
-		return (free_stuff(&stack));
-	while (stack != NULL && stack[j])
-	{
-		new_stack[j] = stack[j];
-		j++;
-	}
-	j = 0;
-	stack = free_stuff(&stack);
-	while (j < bytes_read)
-		new_stack[i++] = buffer[j++];
-	new_stack[i] = 0;
-	return (new_stack);
-}
-
-char	*ft_remove_line(char *stack, char *line)
-{
-	char	*new_stack;
-	size_t	line_len;
-	size_t	stack_len;
-
-	line_len = ft_strlen_buf(line);
-	stack_len = ft_strlen_buf(stack);
-	if (line_len >= stack_len)
-		return (free_stuff(&stack));
-	new_stack = ft_strdup(stack + line_len);
-	free_stuff(&stack);
-	return (new_stack);
 }
 
 char	*ft_extract_line(char *stack)
@@ -133,17 +64,98 @@ char	*ft_extract_line(char *stack)
 	return (line);
 }
 
-char	*ft_update_stack(char	**stack)
+char	*ft_strlcat_buf(char *stack, char *buffer, ssize_t bytes_read)
+{
+	ssize_t		i;
+	ssize_t		j;
+	char		*new_stack;
+
+	i = 0;
+	j = 0;
+	new_stack = NULL;
+	if (bytes_read <= 0)
+		return (stack);
+	i = ft_strlen_buf(stack);
+	new_stack = (char *)malloc((i + bytes_read + 1) * sizeof(char));
+	if (!new_stack)
+		return (free_stuff(&stack));
+	while (stack != NULL && stack[j])
+	{
+		new_stack[j] = stack[j];
+		j++;
+	}
+	j = 0;
+	stack = free_stuff(&stack);
+	while (j < bytes_read)
+		new_stack[i++] = buffer[j++];
+	new_stack[i] = 0;
+	return (new_stack);
+}
+
+static size_t	ft_newline_counter(char *stack)
+{
+	size_t	counter;
+	size_t	i;
+
+	i = 0;
+	counter = 0;
+	while (stack[i])
+	{
+		if (stack[i] == '\n')
+			counter++;
+		i++;
+	}
+	return (counter);
+}
+
+static char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	char	*dst;
+	size_t	src_len;
+
+	i = 0;
+	if (!s1)
+		return (NULL);
+	src_len = ft_strlen_buf(s1);
+	dst = (char *)malloc(sizeof(char) * (src_len + 1));
+	if (dst == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+static char	*ft_remove_line(char *stack, char *line)
+{
+	char	*new_stack;
+	size_t	line_len;
+	size_t	stack_len;
+
+	line_len = ft_strlen_buf(line);
+	stack_len = ft_strlen_buf(stack);
+	if (line_len >= stack_len)
+		return (free_stuff(&stack));
+	new_stack = ft_strdup(stack + line_len);
+	free_stuff(&stack);
+	return (new_stack);
+}
+
+static char	*ft_update_stack(char	**stack)
 {
 	char	*line;
 
-	if (!stack || !(*stack))
+	if (!(*stack))
 		return (NULL);
 	line = ft_extract_line(*stack);
 	if (line)
-	{
 		*stack = ft_remove_line(*stack, line);
-	}
+	if (!line)
+		free_stuff(&*stack);
 	return (line);
 }
 
@@ -152,8 +164,6 @@ char	*get_next_line(int fd)
 	char			buffer[BUFFER_SIZE];
 	static char		*stack;
 	ssize_t			bytes_read;
-	size_t			len_stack;
-	size_t			steps;
 
 	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
@@ -167,14 +177,13 @@ char	*get_next_line(int fd)
 				stack = free_stuff(&stack);
 			if (stack && *stack)
 				return (ft_update_stack(&stack));
-			stack = free_stuff(&stack);
-			return (NULL);
+			return (free_stuff(&stack));
 		}
 		stack = ft_strlcat_buf(stack, buffer, bytes_read);
-		if (ft_newline_counter(stack))
+		if (stack && ft_newline_counter(stack))
 			return (ft_update_stack(&stack));
 	}
-	return (NULL);
+	return (free_stuff(&stack));
 }
 
 int	main()
